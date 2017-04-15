b7d8e047acb50a094d063895d0c5271517a3ca34
daynox <doureyd@gmail.com>
2017-04-15 15:44:17 +0200

    Modify App render method by displaying data from list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 class App extends Component {
   render() {
-    const greeting = 'Welcome to React';
-    const user = {
-      firstName: 'Doureyd',
-      lastName: 'Khelalef',
-    };
     return (
       <div className="App">
-        <h2>{greeting + ' ' + user.firstName + ' ' + user.lastName}</h2>
+        {list.map(item => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <spane>{item.points}</spane>
+          </div>
+        ))}
       </div>
     );
   }
