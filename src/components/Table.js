91fd1acaee31c55818a7b1349d0f145424b532ce
daynox <doureyd@gmail.com>
2017-04-16 06:15:07 +0200

    Add reusable Button component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,26 +1,25 @@
 import React from 'react';
+import Button from './Button';
 
 const isSearched = searchTerm => item =>
   !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-const Table = ({list, searchTerm, onDismiss}) => {
-  return (
-    <div>
-      {list.filter(isSearched(searchTerm)).map(item => (
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <spane>{item.points}</spane>
-          <button type="button" onClick={() => onDismiss(item.objectID)}>
-            Dismiss
-          </button>
-        </div>
-      ))}
-    </div>
-  );
-};
+const Table = ({list, searchTerm, onDismiss}) => (
+  <div>
+    {list.filter(isSearched(searchTerm)).map(item => (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <spane>{item.points}</spane>
+        <Button onClick={() => onDismiss(item.objectID)}>
+          Dismiss
+        </Button>
+      </div>
+    ))}
+  </div>
+);
 
 export default Table;
